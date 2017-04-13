e43829e7703afa4ef9060b4c6e9d95f18829b51e
992999 <992999@LONW000I007486.bnppi.priv>
2017-04-13 15:50:00 +0100

    update

---
+++ src/main/java/reassemble/Main.java
@@ -0,0 +1,28 @@
+package reassemble;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		String resourcesFolder = "src/test/resources/" ;
+		String file = resourcesFolder + args[0];
+		try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+			 List<String> entries = in.lines().collect(Collectors.toList());
+			 assert(entries.size(), 2)
+		}
+		
+	}
+
+}
+
+class YourClass {
+	public static int reassemble(String is) {
+		return 1;
+		
+	}
+}
---
+++ src/test/java/reassemble/fragment/submission/ReassembleTest.java
@@ -0,0 +1,15 @@
+package reassemble.fragment.submission;
+
+public class ReassembleTest {
+	
+	@BeforeClass
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testLinesAreGreaterThan2Characters() {
+		
+	}
+
+}
